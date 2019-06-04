a1194a4cf71b2ad5f0190e438b562f2e78f514ae
Alexandrina <lizurchik.a@gmail.com>
2019-06-04 14:34:25 +0300

    feat: add built files

---
+++ precache-manifest.dd13977d76949d7dc03a7b53e50988e5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f14257a004d3f1b127f06d52a56af0c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5241b3d1a79de4c760b6",
+    "url": "/static/css/main.07442018.chunk.css"
+  },
+  {
+    "revision": "151b2ebf983781731b87",
+    "url": "/static/js/2.d3414afe.chunk.js"
+  },
+  {
+    "revision": "5241b3d1a79de4c760b6",
+    "url": "/static/js/main.56aaf560.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
